17eba8dab7d2d01472229e341fb9ed595b2fcbe6
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-04-15 11:13:27 -0500

    Finished exercise.

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace LINQ
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> games = new List<string>
+            {
+                "League of Legends",
+                "Valorant",
+                "Call of Duty",
+                "Counter Strike 2",
+                "Minecraft",
+                "Pokemon",
+                "Grand Theft Auto V",
+            };
+
+            var gamesOrdered = games.OrderBy(game => game.Length);
+
+            Console.WriteLine("Ordered list of games by length of name: ");
+            foreach ( var game in gamesOrdered)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
